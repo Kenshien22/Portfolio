f7fcdaf007ba4dd1a5eeaff01f303b35ea4c9700
Kenshien22 <kennethescuyos22@gmail.com>
2024-06-09 13:16:02 +0800

    First Commit for this portfolio

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+window.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector('#hamburger');
+const menus = document.querySelector('#menus');
+    hamburger.addEventListener('click', () => {
+        if(menus.className.includes("show")){
+            menus.classList.remove("show");
+            else{
+                menus.classList.add("show");
+            }
+        }
+    });
+});
+window.addEventListener("resize"() => {
+    console.log(window.innerWidth);
+})
